1577c6d27a6da5852920f45f0df985caadba3964
rahatolma <rahatolma@gmail.com>
2026-04-25 23:30:51 +0300

    chore: remove scratch files from git tracking

--- _scratch/patch.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/services/client_dal.ts', 'utf8');
-content = content.replace("  const { data } = await supabase", "export const getUserCollectionStatus = async (userId: string) => {\n  const { data } = await supabase");
-fs.writeFileSync('src/services/client_dal.ts', content);
