e8a17ed9e713f7e8bfaa7bb163cedd2421d3960e
rahatolma <rahatolma@gmail.com>
2026-05-15 23:17:36 +0300

    fix(deps): force Vercel NFT to include @swc/helpers in serverless bundle via outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/*': ['./node_modules/@swc/helpers/**/*'],
+  },
   images: {
     remotePatterns: [
       {
